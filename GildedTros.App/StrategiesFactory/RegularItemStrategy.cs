d6c2e6f51893ddf2c37e6f888a40c27ccd922d18
Max Van de Velde <max.vandevelde@elmos.be>
2025-03-30 14:05:51 +0200

    implementation regular & smelly strategy & updated UpdateQuality method

--- GildedTros.App/StrategiesFactory/RegularItemStrategy.cs
+++ GildedTros.App/StrategiesFactory/RegularItemStrategy.cs
@@ -1,10 +1,21 @@
-﻿namespace GildedTros.App.StrategiesFactory
+﻿using System;
+
+namespace GildedTros.App.StrategiesFactory
 {
     public class RegularItemStrategy : IUpdateStrategy
     {
+        // Once the sell by date has passed, Quality degrades twice as fast
         public virtual void Update(Item item)
         {
-            throw new System.NotImplementedException();
+            DecreaseSellIn(item);
+            DecreaseQuality(item, item.SellIn < 0 ? 2 : 1);
+        }
+
+        protected void DecreaseSellIn(Item item) => item.SellIn--;
+
+        protected void DecreaseQuality(Item item, int amount)
+        {
+            item.Quality = Math.Max(0, item.Quality - amount);
         }
     }
 }
--- GildedTros.App/StrategiesFactory/SmellyItemStrategy.cs
+++ GildedTros.App/StrategiesFactory/SmellyItemStrategy.cs
@@ -2,9 +2,12 @@
 {
     public class SmellyItemStrategy : RegularItemStrategy
     {
+        // Smelly items ("Duplicate Code", "Long Methods", "Ugly Variable Names") degrade in Quality twice as fast as normal items
+
         public override void Update(Item item)
         {
-            throw new System.NotImplementedException();
+            DecreaseSellIn(item);
+            DecreaseQuality(item, item.SellIn < 0 ? 4 : 2);
         }
     }
 }
